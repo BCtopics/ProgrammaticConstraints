85aefdd4cfb66c655e37cba4cd8f79b017414487
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-23 10:09:12 -0600

    Project Cleanup

---
+++ ProgrammaticConstraints/ProgrammaticConstraints/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ProgrammaticConstraints
+//
+//  Created by Bradley GIlmore on 5/23/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
